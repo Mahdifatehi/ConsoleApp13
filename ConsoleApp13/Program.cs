dfbbfd8d36c41349ae0f3de179917fcb19230660
Notebook <Notebook@DESKTOP-RTR8GD9>
2023-10-05 23:28:36 +0330

    Add project files.

---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n,i,c=0;
+            Console.WriteLine("adaad ra vared konid!");
+            n = int.Parse(Console.ReadLine());
+            for (i = 1; i <= n; i++)
+                if (n % i == 0) c++;
+            if (c == 2)
+                Console.WriteLine("adaad shoma aval hast!");
+            else
+                Console.WriteLine("adaad shoma aval nist!");
+
+                        
+
+
+        }
+    }
+}
